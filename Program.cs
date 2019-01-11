0c1f1033ca917b87a97430040df4a56d162d1cbe
DanOwen91 <danowen1@outlook.com>
2019-01-11 21:05:08 +0000

    Fibonacci sequnce found using different data structures

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FibonacciSeq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Use an int[] to store results
+            Fibonacci.FibFromToReturnArray(10);
+            Fibonacci.NthNumberArray(11);
+
+            //Use a list to store results
+            Fibonacci.FibFromToReturnList(10);
+            Fibonacci.NthNumberReturnList(10);
+
+            //Use a linkedlist to store results
+            Fibonacci.FibFromToReturnLinkedList(10);
+            Fibonacci.NthNumberReturnLinkedList(10);
+        }
+    }
+}
